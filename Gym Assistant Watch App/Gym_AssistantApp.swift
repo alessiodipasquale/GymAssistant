c590eb35c849739fe0ec9f20382f15d7b5a23be0
Alessio Di Pasquale <38081627+alessiodipasquale@users.noreply.github.com>
2022-11-22 10:37:03 +0100

    Initial Commit

---
+++ Gym Assistant Watch App/Gym_AssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gym_AssistantApp.swift
+//  Gym Assistant Watch App
+//
+//  Created by Alessio Di Pasquale on 22/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Gym_Assistant_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
